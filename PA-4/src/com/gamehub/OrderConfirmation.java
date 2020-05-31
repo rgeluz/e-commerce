35c5ef48ffb82126984237387eefa723b33dedff
Roman <rgeluz22@yahoo.com>
2020-05-30 21:57:59 -0700

    updated order confirmation's search product link

--- PA-4/src/com/gamehub/OrderConfirmation.java
+++ PA-4/src/com/gamehub/OrderConfirmation.java
@@ -166,7 +166,7 @@ public class OrderConfirmation extends HttpServlet {
 			        "<!-- Right-aligned links-->\n" +
 			        "<div class=\"topnav-right\">\n" +
 			          "<!--<a href=\"#search\">Search</a>-->\n" +
-			          "<button class=\"searchBtn\" onClick=\"openSearch()\" style=\"float:left;\"><i class=\"fas fa-search\"></i> Search</button>\n" +
+			          "<button class=\"searchBtn\" onClick=\"location.href='./products/searchproduct.jsp';\" style=\"float:left;\"><i class=\"fas fa-search\"></i> Search</button>\n" +
 			          "<a href=\"shoppingcart\"><i class=\"fas fa-shopping-cart\"></i> Shopping Cart</a>\n" +
 			        "</div>\n" +
 	
